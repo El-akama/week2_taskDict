29f57b1bca320efa4e07260c672c4a309227da2a
El-akama <akmatjanovel@gmail.com>
2020-11-02 15:20:32 +0600

    initial commit

---
+++ taskDictm.py
@@ -0,0 +1,32 @@
+# task1
+# menu = {
+#     'lagman': 120,
+#     'plov': 120,
+#     'borsh': 100
+#     }
+# menu.update({'besh barmak': 130})
+# menu.update({'lagman': 135})
+# del menu['borsh']
+# print(menu)
+
+# task2
+# slova = {'hello': 'hi', 'good': 'nice', 'price': 'cost', 'black': 'dark'}
+# s = slova.items()
+# l = list(s)
+# print(l[-1][-1])
+
+# task3 
+# country = ['German', 'Italy', 'France']
+# town = ['Berlin', 'Rome', 'Paris']
+# slovari = dict(zip(country, town))
+# print(slovari)
+
+# task4
+# dog = {'name': 'Bobik', 'color': 'black', 'age': 5}
+# cat = {'name': 'Barsik', 'color': 'white', 'age': 3}
+# print(f"This is a dog. His name is {dog['name']}. He is {dog['color']} and he is already {dog['age']} years old.")
+# print(f"This is a cat. His name is {cat['name']}. He is {cat['color']} and he is already {cat['age']} years old.")
+
+# task5
+# info = {'1a': ['John', 'Jessica', 'Tom'], '2b': ['Ben', 'Adam', 'Elena']}
+# print(f"This is {info['1a'][-1]}. He is in grade 1a.")
